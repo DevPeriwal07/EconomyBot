6a45ad449f24555b208a3fed024b2be212708fe7
Dev Periwal <96274188+DevPeriwal07@users.noreply.github.com>
2024-07-28 13:31:32 +0530

    feat: improved stats command

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,5 +1,20 @@
-const { EmbedBuilder } = require('discord.js');
-const { SlashCommandBuilder } = require('discord.js');
+const query = require('../utils/mysql').query;
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+const formatDuration = (ms) => {
+  if (ms < 0) ms = -ms;
+  const time = {
+    d: Math.floor(ms / 86_400_000),
+    h: Math.floor(ms / 3_600_000) % 24,
+    m: Math.floor(ms / 60_000) % 60,
+    s: Math.floor(ms / 1_000) % 60,
+    ms: Math.floor(ms) % 1_000,
+  };
+  return Object.entries(time)
+    .filter((val) => val[1] !== 0)
+    .map(([key, val]) => `${val}${key}`)
+    .join(', ');
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,8 +24,25 @@ module.exports = {
     private: true,
   },
   async run(client, interaction) {
+    const [{ userCount }] = await query(
+      'SELECT COUNT(id) AS userCount FROM users;',
+    );
+    const [{ commandCount }] = await query(
+      'SELECT SUM(commandsRan) AS commandCount FROM users;',
+    );
+
+    console.log(userCount);
     const embed = new EmbedBuilder()
-      .setDescription(`**Uptime:** ${client.uptime}ms`)
+      .setAuthor({
+        name: `${client.user.displayName}'s Stats`,
+        iconURL: client.user.displayAvatarURL(),
+      })
+      .setDescription(
+        `**Uptime:** ${formatDuration(client.uptime)}\n` +
+          `**Total User Count:** ${userCount.toLocaleString()}\n` +
+          `**Total Commands Ran:** ${commandCount.toLocaleString()}`,
+      )
+      .setColor('Random')
       .setTimestamp();
 
     await interaction.reply({ embeds: [embed] });
